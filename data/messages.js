ec3a3a67c2664729c67ccacf581a33d167e9652b
rgarciamvm <91730364+rgarciamvm@users.noreply.github.com>
2022-04-01 16:24:24 +0200

    Add files via upload

---
+++ data/messages.js
@@ -0,0 +1,20 @@
+export default 
+    {
+        router_error:"this URL does not exist or we did not find any resource",
+        parameter_not_especified: 'The parameter was required but was not specified',
+        movie_dosent_exist: 'Ups! Movie does not exist',
+        movie_error_update:'Ups! Can not update the movie',
+        movie_error_create:'Ups! Can not create the movie',
+        actor_error_update:'Ups! Can not update the actors',
+        actor_error_create:'Ups! Can not create the actors',
+        user_exist_register: 'UPSS!! User Already Exist. Please Login',
+        user_error_register:'There was an error in registering a user for site',
+        user_error_login:'Invalid Login Name and/or Password',
+        user_error_active: 'WARNING!! Activate the inactive user profile',
+        user_error_username:'Invalid user Name',
+        user_invalid_format:'Input user name is not in the correct format',
+        passwd_invalid_format:'The password does not meet the password policy requirements'
+
+               
+
+    }
